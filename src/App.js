cffbe47c2c95a09504de61fd7c981869a052717a
Samin <86338112+samxn@users.noreply.github.com>
2023-01-09 19:32:01 -0500

    Added form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Samin</footer>
+      </div>
     </div>
   );
 }
